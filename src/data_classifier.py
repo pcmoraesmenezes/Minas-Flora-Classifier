e64c562edfea49e9dd729f43ecf542d939a6a214
Paulo Moraes <paulo.moraes@sou.unifal-mg.edu.br>
2025-08-15 12:17:12 -0300

    [feat] - classify method

--- src/data_classifier.py
+++ src/data_classifier.py
@@ -17,5 +17,18 @@ class MinasFloraClassifier:
         logging.info(f"Model {model_name} loaded successfully.")
         
         
-    def cosine_similarity(self, x, y):
+    def _get_winner_index(self, x, y):
         return torch.argmax((x @ y) / (torch.linalg.norm(x) * torch.linalg.norm(y)), dim=-1)
+    
+    
+    def classify(self, image, all_labels):
+        if not isinstance(image, Image.Image):
+            raise ValueError("Input must be a PIL Image.")
+        
+        inputs = self.processor(text=all_labels, images=image, return_tensors="pt", padding=True)
+        outputs = self.model(**inputs)
+        
+        winner_idx = self._get_winner_index(outputs.image_embeds, outputs.text_embeds)
+        winner_label = all_labels[winner_idx.item()]
+        logging.info(f"Classified image with label: {winner_label}")
+        return winner_label
